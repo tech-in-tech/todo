5589875ef4f759495d6c0d30402dcf4bff6142a2
tech-in-tech <anubhavsharma06dec@gmail.com>
2024-05-01 17:24:30 +0530

    setup new project (todo app) using react config talwind css

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1 className='text-3xl'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores dignissimos incidunt dolorem amet illum similique provident cupiditate reiciendis velit facere ut sunt est vel dolorum repellendus nam, assumenda aspernatur veniam.</h1>
+    </>
+  )
+}
+
+export default App
